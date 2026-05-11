c2943ee77f9666bad12dc87d0ea79a37009dd0d3
Hugo140804 <hgugi3nsn@gmail.com>
2026-05-11 18:41:43 +0700

    Class Manusia

--- Manusia.cpp
+++ Manusia.cpp
@@ -4,3 +4,19 @@
 #include <string>
 #include "D:/RelasidanInheritance_0236/Jantung.h"
 
+//membuat class manusia di file header manusia.h
+class Manusia {
+public:
+    //membuat member variabel class manusia
+    std::string name;
+    Jantung varJantung;
+
+    //membuat constructor dan destructor class manusia
+    Manusia(std::string pName) : name(pName) {
+        std::cout << name << "Hidup\n";
+    }
+    ~Manusia() {
+        std::cout << name << "Mati\n";
+    }
+};
+#endif // !MANUSIA_H
