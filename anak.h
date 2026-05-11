c5ecf9003a6eb188b22a6131943400f780006939
Hugo140804 <hgugi3nsn@gmail.com>
2026-05-11 17:45:01 +0700

    END anak.h

--- anak.h
+++ anak.h
@@ -1,15 +1,19 @@
 #ifndef ANAK_H
 #define ANAK_H
 
+#include <iostream>
+using namespace std;
+
 class anak {
 public:
     string nama;
     anak(string pNama) : nama(pNama) {
-        cout << "Anak " << nama << " ada" \n";
+        cout << "Anak " << nama << " ada" << endl;
     }
     
     ~anak() {
-        cout << "Anak " << nama << " tidak ada" \n";
+        cout << "Anak " << nama << " tidak ada" << endl;
     }
 };
+#endif
 
