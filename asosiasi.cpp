61b0b21c310de5a11bbaddb00d817b97a7af3b56
Hugo140804 <hgugi3nsn@gmail.com>
2026-05-11 18:30:50 +0700

    class pasien

--- asosiasi.cpp
+++ asosiasi.cpp
@@ -5,3 +5,25 @@ using namespace std;
 //deklarasi class dokter
 class dokter;
 
+//deklarasi dan pembuatan class pasien
+class pasien {
+public:
+    //deklarasi member variabel dari class pasien
+    string nama;
+    vector<dokter*> daftar_dokter;
+
+    //pembuatan constructor dan destructor dari class pasien
+    pasien(string pNama) :nama(pNama) {
+        cout << "Pasien \"" << nama << "\" ada\n";
+    };
+
+    ~pasien() {
+        cout << "Pasien \"" << nama << "\" tidak ada\n";
+    };
+
+    //deklarasi prosedur tambahDokter() dan cetakDokter()
+    void tambahDoker(dokter*);
+    void cetakDokter();
+
+};
+
